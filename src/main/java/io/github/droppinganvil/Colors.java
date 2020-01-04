a75c39d8d2ef84efdce732ec8661c5998e465d63
droppinganvil <dr0pping.4nvi1@gmail.com>
2020-01-04 17:15:34 -0600

    Initial commit

---
+++ src/main/java/io/github/droppinganvil/Colors.java
@@ -0,0 +1,23 @@
+package io.github.droppinganvil;
+
+public enum Colors {
+    DARK_RED("&4"),
+    LIGHT_RED("&c"),
+    GOLD("&6"),
+    YELLOW("&e"),
+    DARK_GREEN("&2"),
+    LIGHT_GREEN("&a"),
+    AQUA("&b"),
+    DARK_AQUA("&3"),
+    DARK_BLUE("&1"),
+    LIGHT_BLUE("&9"),
+    LIGHT_PURPLE("&d"),
+    DARK_PURPLE("&5"),
+    WHITE("&f"),
+    LIGHT_GREY("&7"),
+    DARK_GREY("&8"),
+    BLACK("&0");
+
+    Colors(String s) {
+    }
+}
---
+++ src/main/java/io/github/droppinganvil/SeamlessChat.java
@@ -0,0 +1,27 @@
+package io.github.droppinganvil;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SeamlessChat extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        if (e.getMessage().contains("&")) {
+            String temp = e.getMessage();
+            for (Colors color : Colors.values()) {
+                if (e.getMessage().contains(color.toString()) && e.getPlayer().hasPermission("seamlesschat." + color.name())) {
+                    temp = temp.replace(color.toString(), ChatColor.translateAlternateColorCodes('&', color.toString()));
+                }
+            }
+            e.setMessage(temp);
+        }
+    }
+}
